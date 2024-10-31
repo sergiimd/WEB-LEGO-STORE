67f6de781f9204cb1e68a969a28c53dd8d44fe5d
Álvaro Carrasco Mateos <alvarocm0703@gmail.com>
2024-10-31 02:38:35 +0100

    PÁGINA 2 - APARTADO LADRILLOS Y PREGUNTAS
    Ladrillos y piezas con galería, preguntas frecuentes con acordeón y ventana modal

--- js/script.js
+++ js/script.js
@@ -6,6 +6,37 @@ $(document).ready(function () {
 
 
 
+// LOGO LEGO CAMBIO IMAGEN
+var logo = $("#logolego");
+
+logo.hover(
+    function() {
+    $(this).attr("src", "img/LEGO_logo.svg.png");
+    },
+    function() {
+        $(this).attr("src", "img/logo_navidad.jpg");
+    }
+);
+
+
+// EFECTO GALERÍA
+ScrollReveal().reveal(".grid-wrapper > div", {
+    delay: 200,
+    distance: "50px",
+    interval: 100,
+    origin: "bottom",
+    scale: 0.9,
+});
+
+
+// SLIDER SLICK
+
+  
+
+
+
+
+
 
 
 
