b14aeb060dc73ed610f85f72e1cf677b546a9201
sergiimd <md.sergiio@gmail.com>
2024-11-06 17:12:38 +0100

    AOS y Carrusel Jquery

--- js/script.js
+++ js/script.js
@@ -25,6 +25,23 @@ ScrollReveal().reveal(".grid-wrapper > div", {
     scale: 0.9,
 });
 
-
 // AOS
 AOS.init();
+
+// SLIDER JQUERY
+$(document).ready(function(){
+  
+    var interval = window.setInterval(rotateSlides, 3000)
+    
+    function rotateSlides(){
+      var $firstSlide = $('#carousel').find('div:first');
+      var width = $firstSlide.width();
+      
+      $firstSlide.animate({marginLeft: -width}, 1000, function(){
+        var $lastSlide = $('#carousel').find('div:last')
+        $lastSlide.after($firstSlide);
+        $firstSlide.css({marginLeft: 0})
+      })
+    }
+  
+  })
